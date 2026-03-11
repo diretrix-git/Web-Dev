b5e14f7e1af78387dfab1368b2b82f4535a4591e
diretrix-git <mhrkrish6@gmail.com>
2026-03-11 11:32:39 +0545

    tenth-class:: javascript started

---
+++ tenth-class/script.js
@@ -0,0 +1,42 @@
+// let a = 5;
+// if (true) {
+//   console.log(a);
+
+//   a = 7;
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// let a = 10;
+// let b = 6;
+// const c = 7;
+
+// var sum = a + b;
+// var product = a * b;
+
+//find the reminder of x / y where x = 25 and y = 4
+// var x = 27;
+// var y = 5;
+// var remainder = x % y;
+
+// console.log("The remainder of x / y is: ", remainder);
+// console.log("The sum of a and b is: ", sum);
+// console.log("The product of a and b is: ", product);
+
+// console.log("Hello World");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+let a = 5;
+let b = 6;
+let c = 7;
+
+function sum(x, y) {
+  return x + y;
+}
+
+console.log(sum(a, b));
+console.log(sum(a, c));
+console.log(sum(b, c));
