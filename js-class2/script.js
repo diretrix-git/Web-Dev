4a4acf3a244f369905619b525b7bbe27fd60a5ce
diretrix-git <mhrkrish6@gmail.com>
2026-03-22 11:31:47 +0545

    js-class2 :: use of continue and break

--- js-class2/script.js
+++ js-class2/script.js
@@ -3,11 +3,18 @@ let a = 10;
 let result = a % 2 === 0 ? "even" : "odd";
 console.log(result);
 
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
-}
-
 // for loop to write table of 2
-for (let i = 1; i <= 10; i++) {
-    console.log(`2 x ${i} = ${2 * i}`);
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`2 x ${i} = ${2 * i}`);
+// }
+
+//
+for (let i = 1; i <= 20; i++) {
+  if (i == 3) {
+    continue;
+  }
+  if (i >= 15) {
+    break;
+  }
+  console.log(i);
 }
